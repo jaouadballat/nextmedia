8a9731210bc3cd6f6e093bb1778a1b3de392ce57
jaouad ballat <jaouad.ballat@gmail.com>
2018-09-01 18:10:51 +0100

    finalisation swiper

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,19 +1,54 @@
 <template>
  <div>
-     <swiper ref="awesomeSwiperA" :options="swiperOptionA" @set-translate="onSetTranslate">
+     <swiper ref="awesomeSwiperA" :options="swiperOptionA"  @set-translate="onSetTranslate">
+         <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
       <!-- slides -->
       <swiper-slide v-for="(post, index) in posts" :key="index">
-          {{ post.title }}
+        <header>
+           <div id="back">
+                <router-link to='/'>
+                    <img src="../assets/back.png" alt="back">
+                </router-link>
+           </div>
+           <div id="logo">
+                <img src="../assets/logo2.png" alt="logo">
+            </div>
+            <div id="menu_logo">
+                <a href="#">
+                    <img src="../assets/menu2.png" alt="menu">
+                </a>
+            </div>
+        </header>
+        <div class="card">
+            <div class="image-card">
+                <button class="category">{{ post.categories[0].title }}</button>
+                <router-link :to="{name: 'post', params: {id: post.id}}" tag="a">
+                <img :src="post.thumbnail_images.thumbnail.url" alt="">
+                </router-link>
+            </div>
+            <div class="body-card">
+                <h1><router-link :to="{name: 'post', params: {id: post.id}}" tag="a" style="color:#000">{{ post.title }}</router-link></h1>
+                <p>{{ post.date  }}</p>
+            </div>
+        </div>
+        <div id="social-link">
+                <img src="../assets/facebook.png" alt="">    
+                <img src="../assets/twitter.png" alt="">
+        </div>
+        <div id="content" v-html="post.content"></div>
+        <div id="social-link">
+                <img src="../assets/facebook.png" alt="">    
+                <img src="../assets/twitter.png" alt="">
+        </div>
       </swiper-slide>
-      <!-- Optional controls -->
-      <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
  </div>
 </template>
 
 <script>
-import Api from '../config/Api';
 import { swiper, swiperSlide } from 'vue-awesome-swiper';
+import Api from '../config/Api';
 
 
   export default {
@@ -31,20 +66,85 @@ import { swiper, swiperSlide } from 'vue-awesome-swiper';
             posts: []
         }
      },
+    computed: {
+      swiper() {
+        return this.$refs.awesomeSwiperA.swiper
+      }
+    },
     methods: {
     	onSetTranslate() {
-      	console.log('onSetTranslate')
+      	//console.log('onSetTranslate')
       }
     },
     created() {
-      console.log(this.$route.params.id)
+      //console.log(this.$route.params.id)
         Api().get('get_recent_posts/')
             .then(response => {
-                console.log(response.data);
-                this.posts = [...response.data.posts]
+
+            let { posts } = response.data;
+            let post = response.data.posts.filter(p => p.id === parseInt(this.$route.params.id));
+
+            //index of the current post
+            let index = posts.indexOf(...post);
+            
+            this.posts = [...posts];
+            // Slide to the current post   
+            this.swiper.slideTo(index, 1000, false);
+
             }).catch(error => {
                 console.log(error.response);
             })
     }
   }
 </script>
+
+<style>
+#logo {
+    border-bottom: 1px solid #dfdfdf;
+  }
+
+  #logo img{
+    height: 50px;
+    width: 20%;
+    margin: 0 auto;
+    display: block;
+  }
+
+  #menu_logo {
+    position: absolute;
+    top: 20px;
+    right: 10px;
+  }
+
+  #back {
+      position: absolute;
+      top: 15px;
+      left: 10px;
+      height: 20px;
+      width: 20px;
+      cursor: pointer;
+  }
+
+  #back img {
+      width: 100%;
+      height: 100%;
+  }
+  div#social-link {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
+
+div#social-link img {
+    width: 45%;
+    height: 100%;
+    cursor: pointer;
+    
+} 
+#content {
+    padding: 20px;
+    text-align: end;
+}
+</style>

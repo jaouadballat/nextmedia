304a782acebe4b07a10103f042abae81e4cc7e37
jaouad ballat <jaouad.ballat@gmail.com>
2018-09-01 15:26:56 +0100

    setup swiper

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>
-        post
-    </div>
+ <div>
+     <swiper ref="awesomeSwiperA" :options="swiperOptionA" @set-translate="onSetTranslate">
+      <!-- slides -->
+      <swiper-slide v-for="(post, index) in posts" :key="index">
+          {{ post.title }}
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+ </div>
 </template>
+
+<script>
+import Api from '../config/Api';
+import { swiper, swiperSlide } from 'vue-awesome-swiper';
+
+
+  export default {
+      components: {
+        LocalSwiper: swiper,
+    	LocalSlide: swiperSlide,
+     },
+     data() {
+         return {
+            swiperOptionA: {
+            pagination: {
+                el: '.swiper-pagination'
+                }
+            },
+            posts: []
+        }
+     },
+    methods: {
+    	onSetTranslate() {
+      	console.log('onSetTranslate')
+      }
+    },
+    created() {
+      console.log(this.$route.params.id)
+        Api().get('get_recent_posts/')
+            .then(response => {
+                console.log(response.data);
+                this.posts = [...response.data.posts]
+            }).catch(error => {
+                console.log(error.response);
+            })
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+import './assets/style.css';
+
 import App from './App.vue'
 import router from './routes';
+
+Vue.use(VueAwesomeSwiper);
 
 Vue.use(VueRouter);
 
